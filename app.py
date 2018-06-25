fbf040408e65db9c0c60437f1be5435eefd730e2
Daniel Gladstone <gladstone.d@gmail.com>
2018-06-25 16:41:23 -0400

    make it better

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
+# from flask import Flask
+# from requests import get
+
+# app = Flask('__main__')
+# SITE_NAME = 'https://artpigeon.nyc/'
+
+
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def proxy(path):
+#     return get(f'{SITE_NAME}{path}').content
+
+
+# app.run(host='0.0.0.0', port=8080)
 from flask import Flask
-from requests import get
+from datetime import datetime
+app = Flask(__name__)
 
-app = Flask('__main__')
-SITE_NAME = 'https://artpigeon.nyc/'
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def proxy(path):
-    return get(f'{SITE_NAME}{path}').content
+    <img src="http://loremflickr.com/600/400">
+    """.format(time=the_time)
 
-
-app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True, host='0.0.0.0', port=8080)
