4614ba8b0ecd77d349cf8022b5e3b5814cf8dbea
Daniel Gladstone <gladstone.d@gmail.com>
2018-06-26 20:19:15 -0400

    init;

--- app.py
+++ app.py
@@ -6,56 +6,18 @@ import requests
 from flask import Blueprint, request, Response
 from flask import Flask
 
-URL = 'https://portal.bitcasa.com/login'
+HOST = 'https://art-pigeon.com/'
 
 proxy = Blueprint('proxy', __name__)
 
 
-HTML_REGEX = re.compile(r'((?:src|action|href)=["\'])/')
-JQUERY_REGEX = re.compile(r'(\$\.(?:get|post)\(["\'])/')
-JS_LOCATION_REGEX = re.compile(r'((?:window|document)\.location.*=.*["\'])/')
-CSS_REGEX = re.compile(r'(url\(["\']?)/')
-
-REGEXES = [HTML_REGEX, JQUERY_REGEX, JS_LOCATION_REGEX, CSS_REGEX]
+def get_csrftoken_from_headers(headers):
+    cookie = headers.get('cookie')
+    reg = re.search('csrftoken=(.*);', cookie)
+    return reg.group(1)
 
 
-@proxy.route('/', defaults={'path': ''}, methods=["GET", "POST"])
-@proxy.route('/<path:path>', methods=["GET", "POST"])
-def proxy_request(path, file=""):
-    # host = 'http://127.0.0.1:8000/' + path
-    host = 'https://art-pigeon.com/' + path
-    client = requests.session()
-    client.get(host)  # sets cookie
-    print(type(request.headers.get('cookie')))
-    cookie = request.headers.get('cookie').split(';')[0].split('=')[1]
-    print('new cookie %s' % cookie)
-    # print(type(client.cookies))
-    # csrftoken = client.cookies['csrftoken']
-    if 'csrftoken' in client.cookies:
-        print('cookie token = %s' % client.cookies['csrftoken'])
-        csrftoken = client.cookies['csrftoken']
-        csrftoken = cookie
-    else:
-        csrftoken = None
-    if request.method == "GET":
-        response = client.get(host, cookies=request.cookies)
-    elif request.method == "POST":
-        # response = requests.post(host, data=request.form)
-        post_data = {}
-        print('form token = %s' % request.form['csrfmiddlewaretoken'])
-        for item in request.form:
-            post_data[item] = request.form[item]
-        post_data['csrfmiddlewaretoken'] = csrftoken
-        response = client.post(
-            host,
-            data=post_data,
-            headers=dict(Referer=URL),
-            cookies=request.cookies)
-    proxy_response = Response(
-        response=response.content,
-        status=response.status_code,
-        content_type=response.headers['content-type'])
-
+def set_cookies_in_proxy_response(response, proxy_response):
     for cookie in response.cookies:
         proxy_response.set_cookie(
             key=cookie.name,
@@ -63,6 +25,40 @@ def proxy_request(path, file=""):
             path=cookie.path,
             expires=cookie.expires,
             secure=cookie.secure)
+
+
+def get_post_response(request, client, csrftoken, url):
+    post_data = {}
+    for item in request.form:
+        post_data[item] = request.form[item]
+    post_data['csrfmiddlewaretoken'] = csrftoken
+    response = client.post(
+        url,
+        data=post_data,
+        headers=dict(Referer=url),
+        cookies=request.cookies)
+    return response
+
+
+@proxy.route('/', defaults={'path': ''}, methods=["GET", "POST"])
+@proxy.route('/<path:path>', methods=["GET", "POST"])
+def proxy_request(path, file=""):
+    url = HOST + path
+    client = requests.session()
+    client.get(url)
+    csrftoken = get_csrftoken_from_headers(request.headers)
+
+    if request.method == "GET":
+        host_response = client.get(url, cookies=request.cookies)
+    elif request.method == "POST":
+        host_response = get_post_response(request, client, csrftoken, url)
+    proxy_response = Response(
+        response=host_response.content,
+        status=host_response.status_code,
+        content_type=host_response.headers['content-type'])
+
+    set_cookies_in_proxy_response(host_response, proxy_response)
+
     return proxy_response
 
 app = Flask(__name__)
--- static/js/jquery.mobile_nav.js
+++
@@ -1,9 +0,0 @@
-var width = $(window).width(); 
-var height = $(window).height(); 
-
-if (width >= 480) {
- //do something
-}
-else {
-//do something else
-}
